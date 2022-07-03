ca53ee77c0c490fbec4a84a6ff7e1c8bd13704a4
Olajide olanrewaju <olajide.m.olanrewaju@gmail.com>
2022-07-03 15:23:35 +0100

    initialised calculator

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { Component } from "react/cjs/react.development";
+import "./App.css";
+
+export default class App extends Component {
+
+  render() {
+    return (
+    <>dd</>
+    );
+  }
+}
